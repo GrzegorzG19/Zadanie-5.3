0912c07652ec2afb0521fe16592dedc2f49a5f3d
Grzegorz <grzegorzgolan18@gmail.com>
2019-05-21 14:48:09 +0200

    v4

--- src/Longer.java
+++ src/Longer.java
@@ -1,6 +1,6 @@
 public class Longer {
 
-    int ifLong(Line lin1, Line lin2){
+    Line ifLong(Line lin1, Line lin2){
 
         int odc1 = lin1.p2.start - lin1.p1.start;
         int odc2 = lin2.p2.start - lin1.p1.start;
@@ -8,20 +8,20 @@ public class Longer {
         if(odc1 > odc2){
             System.out.println("odcinek pierwszy jest dłuższy");
             System.out.printf("początek: %d koniec: %d dlugość: %d",lin1.p1.start, lin1.p2.start, odc1);
-            return odc1;
+            return lin1;
         }
         else if(odc1 == odc2)
         {
             System.out.print("odcinki są równe");
             System.out.printf("początek: %d koniec: %d dlugość: %d\n",lin1.p1.start, lin1.p2.start, odc1);
             System.out.printf("początek: %d koniec: %d dlugość: %d",lin2.p1.start, lin2.p2.start, odc2);
-            return 0;
+            return lin1;
 
         }
         else{
             System.out.print("odcinek drugi jest dłuższy\n");
-            System.out.printf("początek: %d koniec: %d dlugość: %d",lin2.p1.start, lin2.p2.start, odc1);
-            return odc2;
+            System.out.printf("początek: %d koniec: %d dlugość: %d",lin2.p1.start, lin2.p2.start, odc1  );
+            return lin2;
         }
     }
 }
